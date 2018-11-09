d8bead65a8df307aead7ebf7fb15004d0eab81f1
Dias Zhassanbay <dias@Diass-MacBook-Pro.local>
2018-11-10 01:25:59 +0600

    Added functions and minor changes

--- webscraper.py
+++ webscraper.py
@@ -3,21 +3,31 @@ import csv
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+def find_price(stock_ticker):
 
-stock_ticker = input("Enter a stock ticker: ")
+    url = "http://kase.kz/ru/shares/show/" + stock_ticker + "/"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
 
-url = "http://kase.kz/ru/shares/show/" + stock_ticker + "/"
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, "html.parser")
-
-ticker = soup.find("h1", attrs={"style": "text-transform: none;"})
-ticker = ticker.text.strip()
+    ticker = soup.find("h1", attrs={"style": "text-transform: none;"})
+    ticker = ticker.text.strip()
 
 
-price = soup.find("div", attrs={"class": "h1-block__indicator"})
-price = price.text.strip()
-print("Price of " + stock_ticker + ": " + price)
+    price = soup.find("div", attrs={"class": "h1-block__indicator"})
+    price = price.text.strip()
 
-with open("stock.csv", 'a') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow([ticker, price, datetime.now()])
+    return price
+
+def append_data(stock_ticker, price):
+    with open("stock.csv", 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([ticker, price, datetime.now()])
+
+
+ticker = input("Enter a stock ticker: ")
+try:
+    price = find_price(ticker)
+    print("Price of " + ticker + ": " + price)
+    append_data(ticker, price)
+except:
+    print("No stock with such ticker")
