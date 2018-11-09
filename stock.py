8e8f7de0612ca958bc5d79809318f99e688624a9
Dias Zhassanbay <dias@Diass-MacBook-Pro.local>
2018-11-10 02:16:49 +0600

    Added new class Stock

---
+++ stock.py
@@ -0,0 +1,33 @@
+import csv
+from datetime import datetime
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+class Stock:
+
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.price = -1
+
+    def find_price(self):
+
+        url = "http://kase.kz/ru/shares/show/" + self.ticker + "/"
+        page = urlopen(url)
+        soup = BeautifulSoup(page, "html.parser")
+
+        ticker = soup.find("h1", attrs={"style": "text-transform: none;"})
+        ticker = ticker.text.strip()
+
+
+        price = soup.find("div", attrs={"class": "h1-block__indicator"})
+        price = price.text.strip()
+
+        self.price = price
+
+    def append_data(self):
+        with open("stock.csv", 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([self.ticker, self.price, datetime.now()])
+
+    def __str__(self):
+        return "Price of " + self.ticker + ": " + self.price
--- webscraper.py
+++ webscraper.py
@@ -1,33 +1,9 @@
-import urllib
-import csv
-from datetime import datetime
-from bs4 import BeautifulSoup
+from stock import Stock
 
-def find_price(stock_ticker):
-
-    url = "http://kase.kz/ru/shares/show/" + stock_ticker + "/"
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, "html.parser")
-
-    ticker = soup.find("h1", attrs={"style": "text-transform: none;"})
-    ticker = ticker.text.strip()
-
-
-    price = soup.find("div", attrs={"class": "h1-block__indicator"})
-    price = price.text.strip()
-
-    return price
-
-def append_data(stock_ticker, price):
-    with open("stock.csv", 'a') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([ticker, price, datetime.now()])
-
-
-ticker = input("Enter a stock ticker: ")
+stock = Stock(input("Enter a stock ticker: "))
 try:
-    price = find_price(ticker)
-    print("Price of " + ticker + ": " + price)
-    append_data(ticker, price)
+    stock.find_price()
+    stock.append_data()
+    print(stock)
 except:
     print("No stock with such ticker")
